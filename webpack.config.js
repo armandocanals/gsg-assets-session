6421939eb4a57c6de8ebde8a31c76992cc601105
Armando Canals <armandocanals@gmail.com>
2018-11-11 12:31:29 -0800

    JS/webpack assets code example

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss|.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test:/\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
