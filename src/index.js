d1f8ba25dd713633093f36a69c6e13fc4669e255
Armando Canals <armandocanals@gmail.com>
2018-11-11 20:37:24 -0800

    Fix example-1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
-import _ from 'lodash';
-import {add, subtract} from './math';
-import "./main.scss";
-import Logo from "./node_logo.svg";
-
-let lodashOutput = document.getElementById("lodash-output");
-let addOutput = document.getElementById("add-output");
-let subtractOutput = document.getElementById("subtract-output");
-let imageContainer = document.createElement('div');
-let myLogo = new Image();
-
-lodashOutput.innerHTML = _.join(['Hello', 'from', 'the', 'lodash', 'library.'], ' ');
-addOutput.innerHTML = add(2,2);
-subtractOutput.innerHTML = subtract(2,1);
-
-myLogo.src = Logo;
-imageContainer.appendChild(myLogo);
-document.body.appendChild(imageContainer);
+import lodash from 'lodash';
+import {add} from './math';
+console.log(_.join(['Hello', 'from', 'the', 'lodash', 'library.'], ' '));
+console.log(add(2,2));
