c423217355fc74307c3ee993e4ade76b3427cb30
Armando Canals <armandocanals@gmail.com>
2018-11-11 20:58:54 -0800

    Finish example-1

--- dist/main.js
+++ dist/main.js
@@ -127,7 +127,7 @@ eval("module.exports = function(module) {\n\tif (!module.webpackPolyfill) {\n\t\
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _math__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./math */ \"./src/math.js\");\n\n\nconsole.log(_.join(['Hello', 'from', 'the', 'lodash', 'library.'], ' '));\nconsole.log(Object(_math__WEBPACK_IMPORTED_MODULE_1__[\"add\"])(2,2));\n\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _math__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./math */ \"./src/math.js\");\n\n\nconsole.log(lodash__WEBPACK_IMPORTED_MODULE_0___default.a.join(['Hello', 'from', 'the', 'lodash', 'library.'], ' '));\nconsole.log(Object(_math__WEBPACK_IMPORTED_MODULE_1__[\"add\"])(2,2));\nconsole.log(Object(_math__WEBPACK_IMPORTED_MODULE_1__[\"subtract\"])(2,1));\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import lodash from 'lodash';
-import {add} from './math';
+import _ from 'lodash';
+import {add, subtract} from './math';
 console.log(_.join(['Hello', 'from', 'the', 'lodash', 'library.'], ' '));
 console.log(add(2,2));
+console.log(subtract(2,1));
+
